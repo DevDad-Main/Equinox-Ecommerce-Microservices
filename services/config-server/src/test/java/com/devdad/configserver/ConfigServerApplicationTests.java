7b24b74d95a6994be12adb38e38dfdbf3f53162b
Oliver Metz <softwaredevdad@gmail.com>
2026-05-22 17:22:00 +0200

    feat: Config Service.
    - add boilerplate config-server service with one dependency of
    config-server.

---
+++ services/config-server/src/test/java/com/devdad/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdad.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
