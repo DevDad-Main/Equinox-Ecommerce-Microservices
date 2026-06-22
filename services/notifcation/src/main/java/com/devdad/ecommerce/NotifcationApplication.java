46af1055521314bab19163981866b0851896d3e2
Oliver Metz <softwaredevdad@gmail.com>
2026-06-22 09:32:35 +0200

    feat: Notification Service Skeleton.
    - add notification service skeleton code.

---
+++ services/notifcation/src/main/java/com/devdad/ecommerce/NotifcationApplication.java
@@ -0,0 +1,13 @@
+package com.devdad.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifcationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifcationApplication.class, args);
+	}
+
+}
